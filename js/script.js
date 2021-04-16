b7991f097d3b63a76f0f834a5979f13c74b1597a
Rohit <rohitsraj12@gmail.com>
2021-04-16 14:22:12 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var carousel = $('.owl-carousel').owlCarousel({
+    $('.home_banner').owlCarousel({
         items: 1,
         margin: 10,
         nav: false,
@@ -10,13 +10,35 @@ $(document).ready(function(){
         navSpeed: 1500,
         animateIn: 'fadeIn',
         animateOut: 'fadeOut',
-        responsive: {
-            600: { 
+        // responsive: {
+            // 600: { 
                 // animateOut: 'fadeOut',
                 // animateIn: 'fadeIn',
                 //   dots: true,
+            // },
+        // },
+        
+      });
+
+
+      $('.service__slider').owlCarousel({
+        items: 1,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        loop: true,
+        navSpeed: 1500,
+        navText:["<div class='d-none nav-btn prev-slide'></div>","<div class='nav-btn next-slide'><span class='link__icon'>&#8594;</span></div>"],
+        responsive: {
+            600: { 
+                items: 3,
+                  dots: true,
             },
         },
         
       });
+
+
 })
