2b7e4fa6c733cacc7ea0f74910784956c6ef932b
rohitsraj12 <rohitsraj12@gmail.com>
2021-04-30 03:16:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    $(".nav__link").hover(function(){
+      $(this).parent().find(".sub__nav").slideDown();
+    })
+
+    $(".sub__nav").mouseleave(function(){
+      $(this).slideUp();
+    })
+
     $('.home_banner').owlCarousel({
         items: 1,
         margin: 10,
@@ -23,16 +31,21 @@ $(document).ready(function(){
 
       $('.service__slider').owlCarousel({
         items: 1,
-        margin: 10,
-        nav: false,
+        margin: 5,
+        nav: true,
         dots: false,
         autoplay: true,
-        autoplayTimeout: 6000,
+        autoplayHoverPause: true,
+        autoplayTimeout: 3000,
         loop: true,
         navSpeed: 1500,
-        navText:["<div class='d-none nav-btn prev-slide'></div>","<div class='nav-btn next-slide'><span class='link__icon'>&#8594;</span></div>"],
+        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
         responsive: {
             600: { 
+                items: 2,
+                  dots: true,
+            },
+            1200: { 
                 items: 3,
                   dots: true,
             },
@@ -40,5 +53,54 @@ $(document).ready(function(){
         
       });
 
+      // 2nd slider testimonial
+    var $owl = $('.section__testimonial .owl-carousel');
+
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
+
+    $owl.owlCarousel({
+      // center: true,
+      loop: true,
+      items: 3,
+      autoplay: true,
+      autoplayTimeout: 6000,
+      autoplayHoverPause: true,
+        dots:false,
+      // autoplayHoverPause: true,
+        nav: false,
+        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        },
+
+    }
+    });
+
+    $(document).on('click', '.owl-item>div', function() {
+      // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
+      var $speed = 300;  // in ms
+      $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+    });
+
+    $('.event__block').hover(function(){
+      $('.community__front > .gallery__block').toggleClass('gallery__block-body');
+    });
+
+    // faq toggle
+    $('.faq__header').click(function(){
+      $('.faq__ans').slideUp(300);
+      $('.fa-angle-down').css('transform', 'rotate(0deg)');
+      $(this).parent().find('.faq__ans').slideToggle(300);
+      $(this).parent().find('.fa-angle-down').css('transform', 'rotate(180deg)');
+    });
 
 })
