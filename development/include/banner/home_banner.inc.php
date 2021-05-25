40e51a00cf194cc68e77ee9b665f54e987c2c806
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-25 22:24:02 +0530

    Create home_banner.inc.php

---
+++ development/include/banner/home_banner.inc.php
@@ -0,0 +1,75 @@
+
+
+        <!-- start body banner -->
+        <div class="body__banner">
+            <div class="owl-carousel home_banner owl-theme">
+                <div class="item">
+                    <!-- start banner image -->
+                    <div class="banner__image">
+                        <img class="banner__image-block" src="img/banner/banner_lg_2.jpg" alt="banner ">
+                    </div>
+                    <!-- end banner image -->
+
+                    <!-- start banner content -->
+                    <div class="banner__content">
+                        <div class="banner__content-wrap wrap__container home__banner-content">
+                            <section>
+                                <header class="">
+                                    <h2 class="banner_header-one">
+                                        Commercial
+                                    </h2>
+                                    <h3 class="banner_header-two">
+                                        Private Modern Office
+                                    </h3>
+                                </header>
+                                
+
+                                <!-- <div class="banner__btn-wrap">
+                                    <a href="" class="banner__btn">view projects</a>
+                                </div> -->
+                            </section>
+                        </div>
+                    </div>
+                    <!-- end banner content -->
+
+                </div>
+
+                <div class="item bg-danger">
+                    <!-- start banner image -->
+                    <div class="banner__image">
+                        <img class="banner__image-block" src="img/banner/banner_lg_1.jpg" alt="">
+                    </div>
+                    <!-- end banner image -->
+
+                    <!-- start banner content -->
+                    <div class="banner__content">
+                        <div class="banner__content-wrap wrap__container home__banner-content">
+                            <section>
+                                <header class="">
+                                    <h2 class="banner_header-one">
+                                        Residential
+                                    </h2>
+                                    <h3 class="banner_header-two">
+                                        Private Modern Residence
+                                    </h3>
+                                </header>
+                                
+
+                                <!-- <div class="banner__btn-wrap">
+                                    <a href="" class="banner__btn">view projects</a>
+                                </div> -->
+                            </section>
+                        </div>
+                    </div>
+                    <!-- end banner content -->
+                </div>
+                
+            </div>
+
+            <section class="example">
+                <a href="#body-container" class="scroll-icon">
+                    <span class="scroll-icon__dot"></span>
+                </a>
+            </section>
+        </div>
+        <!-- end body banner -->
