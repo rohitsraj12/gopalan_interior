75f48526c03bb034ce2fa3189ef1c37a56d95f58
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-26 02:11:40 +0530

    Update portfolio.php

--- development/portfolio.php
+++ development/portfolio.php
@@ -58,186 +58,84 @@
         
 
         <!-- start body container -->
-        <div class="body__container" id="body-container">
+        <div class="body__container pb-5 mb-5" id="body-container">
             <main>
-                <!-- start section about -->
-                <section class="section__block section__about">
-                    <div class="container">
-                        <div class="row align-items-center">
-                            <div class="col-12 col-md-5"
-                            data-aos="fade-up"
-                            data-aos-offset="100"
-                            data-aos-delay="150"
-                            data-aos-duration="1000"
-                            data-aos-easing="ease-in-out">
-                                <figure>
-                                    <img class="w-100" src="img/draft/story-2.jpg" alt="">
-                                </figure>    
-                            
-                            </div>
-                            <div class="col-12 col-md-1" ></div>
-                            <div class="col-12 col-md-6"  
-                            data-aos="fade-up"
-                            data-aos-offset="100"
-                            data-aos-delay="50"
-                            data-aos-duration="1000"
-                            data-aos-easing="ease-in-out">
-                                <div class="wrap__header">
-                                    <h2 class="section__header-primary">
-                                        Project name
-                                    </h2>
-                                    <h2 class="section__header-main">
-                                        Project details goes here
-                                    </h2>
-                                </div>
-                                
-                                <div class="wrap__detail">
-                                    <!-- <p class="sub__detail">
-                                        Lorem ipsum dolor sit a tenetur magni amet similique, excepturi praesentium vero odio vel voluptas.
-                                    </p> -->
 
-                                    <!-- <p class="main__detail">
-                                        Lorem,tore asperiores mollitia et nostrum? Nostrum fuga autem optio dolor, provident explicabo deleniti laboriosam neque repudiandae modi dolorem amet distinctio odit odio delectus cumque illo vitae assumenda aut!
-                                    </p>
-
-                                    <p class="main__detail">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit enim quod possimus. Nostrum ea debitis laudantium earum iste quisquam quia.
-                                    </p>
-
-                                    
-                                    <ul class="service__points main__detail">
-                                        <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
-                                        <li>Lorem ipsum dolor sit amet consectetur.</li>
-                                        <li>Lorem ipsum dolor sit amet consectetur adipi</li>
-                                        <li>Lorem ipsum dolor sit.</li>
-                                    </ul> -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <!-- end section about -->
-
-                <!-- start section testimonial -->
-                <section class="section__testimonial">
-                    
-                    <div class="wrap__testimonial container">
-                        <header class="pb-3" 
-                        data-aos="fade-right"
-                        data-aos-offset="200"
-                        data-aos-delay="50"
-                        data-aos-duration="1000"
-                        data-aos-easing="ease-in-out">
-                            <h2 class="section__header-primary text-white pb-3">
-                                Our Happy client
+                <section class="section__block section__gallery-home">
+                    <div class="wrap__container">
+                        <div class="wrap__header py-5">
+                            <h2 class="section__header-primary">
+                                Portfolio
                             </h2>
-                        </header>
-
-                        <div class="owl-carousel text-white" 
-                        data-aos="fade-left"
-                        data-aos-offset="200"
-                        data-aos-delay="50"
-                        data-aos-duration="1000"
-                        data-aos-easing="ease-in-out">
-                            <article >
-                                <blockquote class="">
-                                    <div class=" text-center">
-                                        <p class="client__say">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum eum tenetur officiis pariatur doloribus accusantium odio nisi magnam! A hic nesciunt cum culpa earum quasi illum nemo nobis esse repellat.
-                                        </p>
-                                        <div class="client-info d-flex justify-content-center">
-                                            
-                                            <cite>
-                                                <h4 class="client__name">
-                                                    Rajasekhara Reddy A
-                                                </h4>
-                                                <!-- <p class="client__profile">
-                                                    Director
-                                                </p> -->
-                                            </cite>
-                                        </div>
-                                    </div>
-                                </blockquote>
-                            </article>
+                            <h2 class="section__header-main">
+                                Gopalan Interior Portfolio
+                            </h2>
                         </div>
-                    </div>
-                    
-                </section>
-                <!-- end section testimonial -->
-
-                
-
-                <!-- start section story -->
-                <section class="section__block section__story">
-                    <div class="section__group py-5">
-                        <div class="container">
+                        <div class="fx3">
                             <div class="row">
-                                <div class="col-12 col-md-6"  
-                                data-aos="fade-up"
-                                data-aos-offset="200"
-                                data-aos-delay="50"
-                                data-aos-duration="1000"
-                                data-aos-easing="ease-in-out">
-                                    <div class="wrap__header">
-                                        <!-- <h2 class="section__header-primary">
-                                            Story of Gopalan
-                                        </h2> -->
-                                        <h2 class="section__header-main">
-                                            Photo gallery
-                                        </h2>
-                                    </div>
-
-                                    <div class="section__body pt-3 wrap__detail">
-                                      
-                                    </div>
+                                <div class="col-12 col-sm-6 col-md-4  mb-3">
+                                    <a href="project_details.php">
+                                        <div class="item col4">
+                                            <img class="transition img-responsive" src="img/draft/7.jpg">
+                                            <h4>Project Name</h4>
+                                            <p>Kitchen</p>
+                                        </div>
+                                    </a>
                                 </div>
-
-                                
+                                <div class="col-12 col-sm-6 col-md-4  mb-3">
+                                    <a href="project_details.php">
+                                        <div class="item col4">
+                                            <img class="transition img-responsive" src="img/draft/2.jpg">
+                                            <h4>Project Name</h4>
+                                            <p>Living Room</p>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4  mb-3">
+                                    <a href="project_details.php">
+                                        <div class="item col4">
+                                            <img class="transition img-responsive" src="img/draft/5.jpg">
+                                            <h4>Project Name</h4>
+                                            <p>Kitchen</p>
+                                        </div>
+                                    </a>
+                                </div><div class="col-12 col-sm-6 col-md-4  mb-3">
+                                    <a href="project_details.php">
+                                        <div class="item col4">
+                                            <img class="transition img-responsive" src="img/draft/7.jpg">
+                                            <h4>Project Name</h4>
+                                            <p>Kitchen</p>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4  mb-3">
+                                    <a href="project_details.php">
+                                        <div class="item col4">
+                                            <img class="transition img-responsive" src="img/draft/2.jpg">
+                                            <h4>Project Name</h4>
+                                            <p>Living Room</p>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-12 col-sm-6 col-md-4  mb-3">
+                                    <a href="project_details.php">
+                                        <div class="item col4">
+                                            <img class="transition img-responsive" src="img/draft/5.jpg">
+                                            <h4>Project Name</h4>
+                                            <p>Kitchen</p>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
+                                    
+                                    
+
+                        </div> 
                     </div>
 
-                    <!-- <div class="section__group">
-                        <div class="container">
-                            <div class="row align-items-center">
-                                <div class="col-12 col-md-5" 
-                                data-aos="fade-up"
-                                data-aos-offset="200"
-                                data-aos-delay="50"
-                                data-aos-duration="1000"
-                                data-aos-easing="ease-in-out">
-                                    <figure>
-                                        <img class="w-100" src="img/draft/2.jpg" alt="">
-                                    </figure>
-                                </div>
-                                <div class="col-md-1"></div>
-                                <div class="col-12 col-md-6" 
-                                data-aos="fade-up"
-                                data-aos-offset="200"
-                                data-aos-delay="150"
-                                data-aos-duration="1000"
-                                data-aos-easing="ease-in-out">
-                                    <div class="wrap__header">
-                                        <h2 class="section__header-primary">
-                                            Story of Gopalan
-                                        </h2>
-                                        <h2 class="section__header-main">
-                                            Strength Of Planning.
-                                        </h2>
-                                    </div>
-
-                                    <div class="section__body pt-3 wrap__detail">
-                                        <p class="main__detail">
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum enim veritatis autem? Voluptate sapiente autem cum quibusdam tenetur vero magni magnam, aspernatur impedit neque nihil? Quos vitae minima adipisci vel hic. Error, blanditiis dolor atque eaque deleniti mollitia corporis animi magni, vel, nihil autem sunt ea molestias nobis eos debitis.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
 
                 </section>
-                <!-- end section story -->
+
 
                 <!-- <section class="py-5"></section> -->
             </main>
