6530ef7d5ed57cb062b13ed13961c9cbe0593cc3
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-26 02:11:02 +0530

    Create gallery.php

---
+++ development/gallery.php
@@ -0,0 +1,130 @@
+<?php
+    $page_title = "Gallery";
+    $meta_description = "";
+    $meta_keyword = "";
+    $active_nav = "gallery";
+
+
+    include "include/header.inc.php";
+
+?>
+
+
+    
+
+        <!-- start body banner -->
+        <div class="other-body__banner">
+            <div class="banner__image">
+                <img class="other-banner__image-block" src="img/banner/banner_lg_2.jpg" alt="">
+            </div>
+            <!-- end banner image -->
+
+            <!-- start banner content -->
+            <div class="other-banner__content">
+                <div class="banner__content-wrap wrap__container">
+                    <section>
+                        <header class=""  
+                        data-aos="fade-up"
+                        data-aos-offset="100"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out">
+                            <h2 class="banner_header-one">
+                                Gallery
+                            </h2>
+                            <!-- <h3 class="banner_header-two">
+                                Architecture
+                            </h3> -->
+                        </header>
+
+                        <!-- <div class="banner__btn-wrap">
+                            <a href="" class="banner__btn">view projects</a>
+                        </div> -->
+                    </section>
+                </div>
+            </div>
+            <!-- end banner content -->
+        
+
+
+            <!-- <section class="example">
+                <a href="#body-container" class="scroll-icon">
+                    <span class="scroll-icon__dot"></span>
+                </a>
+            </section> -->
+        </div>
+        <!-- end body banner -->
+
+        
+
+        <!-- start body container -->
+        <div class="body__container pb-5 mb-5" id="body-container">
+            <main>
+
+                <section class="section__block section__gallery-home">
+                    <div class="wrap__container">
+                        <div class="wrap__header py-5">
+                            <h2 class="section__header-primary">
+                                Gallery
+                            </h2>
+                            <h2 class="section__header-main">
+                                Gopalan Interior gallery
+                            </h2>
+                        </div>
+                        
+
+                        <div class="section__body pt-3 wrap__detail">
+                                        <div class="row">
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/1.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/2.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/4.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/1.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/2.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/4.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/2.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                            <div class="col-12 col-sm-4 col-md-3 mb-4">
+                                                <img class="mklbItem" src="img/draft/4.jpg" data-gallery="myGal" />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                    </div>
+
+
+                </section>
+
+
+                <!-- <section class="py-5"></section> -->
+            </main>
+        </div>
+        <!-- end body container -->
+
+
+
+
+<?php
+
+include "include/footer.inc.php";
+
+?>
