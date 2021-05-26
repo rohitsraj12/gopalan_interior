6f0deb67e697fd4f0a0ebb8f8136a195132cf66a
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-27 00:58:40 +0530

    Create work_process.php

---
+++ development/include/sections/work_process.php
@@ -0,0 +1,135 @@
+
+                <!-- start section story -->
+                <section class="section__block section__story">
+                    <div class="section__group py-5">
+                        <div class="wrap__container ">
+                            <div class="wrap__header pb-3">
+                                <h2 class="section__header-primary">
+                                    Process
+                                </h2>
+                                <h2 class="section__header-main">
+                                    How we work
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="how_it_work">
+
+                            <div class="wrap__step">
+
+                                <article data-aos="fade-up"
+                                    data-aos-offset="100"
+                                    data-aos-delay="50"
+                                    data-aos-duration="1000"
+                                    data-aos-easing="ease-in-out">
+                                    <div class="step__block d-flex justify-content-between align-items-end">
+                                        <div class="section__left w-75">
+                                            <header class="section__header-primary pb-2">
+                                                Meet your designer
+                                            </header>
+                                            <p class="main__detail">
+                                            Speak to your designer and get personalized quotes and designs for your dream home.   
+                                            </p>
+                                        </div>
+                                        <div class="section__right">
+                                            <span>
+                                                01
+                                            </span>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article data-aos="fade-up"
+                                    data-aos-offset="100"
+                                    data-aos-delay="50"
+                                    data-aos-duration="1000"
+                                    data-aos-easing="ease-in-out">
+                                    <div class="step__block d-flex justify-content-between align-items-end">
+                                        <div class="section__left w-75">
+                                            <header class="section__header-primary pb-2">
+                                                Finalize your Design
+                                            </header>
+                                            <p class="main__detail">
+                                            Get into the nitty-gritties and pick the materials and finishes of your choice.   
+                                            </p>
+                                        </div>
+                                        <div class="section__right">
+                                            <span>
+                                                02
+                                            </span>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article data-aos="fade-up"
+                                    data-aos-offset="100"
+                                    data-aos-delay="50"
+                                    data-aos-duration="1000"
+                                    data-aos-easing="ease-in-out">
+                                    <div class="step__block d-flex justify-content-between align-items-end">
+                                        <div class="section__left w-75">
+                                            <header class="section__header-primary pb-2">
+                                                Place your Order
+                                            </header>
+                                            <p class="main__detail">
+                                            Finalize your design, pay 50% advance and your project is ready for a good start.   
+                                            </p>
+                                        </div>
+                                        <div class="section__right">
+                                            <span>
+                                                03
+                                            </span>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article data-aos="fade-up"
+                                    data-aos-offset="100"
+                                    data-aos-delay="50"
+                                    data-aos-duration="1000"
+                                    data-aos-easing="ease-in-out">
+                                    <div class="step__block d-flex justify-content-between align-items-end">
+                                        <div class="section__left w-75">
+                                            <header class="section__header-primary pb-2">
+                                                Execution and Installation
+                                            </header>
+                                            <p class="main__detail">
+                                                Manufacturing orders in process and delivered on site. 100% payment before installation. 
+                                            </p>
+                                        </div>
+                                        <div class="section__right">
+                                            <span>
+                                                04
+                                            </span>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article data-aos="fade-up"
+                                    data-aos-offset="100"
+                                    data-aos-delay="50"
+                                    data-aos-duration="1000"
+                                    data-aos-easing="ease-in-out">
+                                    <div class="step__block d-flex justify-content-between align-items-end">
+                                        <div class="section__left w-75">
+                                            <header class="section__header-primary pb-2">
+                                            Move In 
+                                            </header>
+                                            <p class="main__detail">
+                                            It’s time to build new memories. Your dream home is now a reality.  
+                                            </p>
+                                        </div>
+                                        <div class="section__right">
+                                            <span>
+                                                05
+                                            </span>
+                                        </div>
+                                    </div>
+                                </article>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </section>
+                <!-- end section story -->
+
+
+                
