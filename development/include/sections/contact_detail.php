068963702dd358ba099a10ae2e117ac0c69acb79
rohitsraj12 <rohitsraj12@gmail.com>
2021-05-31 23:36:05 +0530

    Create contact_detail.php

---
+++ development/include/sections/contact_detail.php
@@ -0,0 +1,61 @@
+
+                <section class="section__book pb-5">
+
+<div class="wrap__container">
+    <header class="pb-3" 
+        data-aos="fade-right"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out">
+        <h2 class="section__header-primary pb-3">
+            Contact
+        </h2>
+    </header>
+
+    <div class="section__body pb-5">
+        <div class="row">
+            <div class="col-12 col-md-4">
+
+                <article>
+                    <header>
+                        Call Us
+                    </header>
+
+                    <div class="book__body text-center">
+                        <a href="tel:08049314931" class="btn">080 4931 4931</a>
+                    </div>
+                </article>
+            </div>
+            <div class="col-12 col-md-4">
+
+                <article>
+                    <header>
+                        Free consultation
+                    </header>
+
+                    <div class="book__body text-center">
+                        <a href="contact_us.html" class="btn btn__primary">Free Consultation</a>
+                    </div>
+                </article>
+            </div>
+            <div class="col-12 col-md-4">
+                <article>
+                    <header>
+                        Follow us
+                    </header>
+                    <ul class="social__media list-unstyled d-flex justify-content-center">
+                        <li class="mx-2"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li class="mx-2"><a href=""><i class="fa fa-twitter"></i></a></li>
+                        <li class="mx-2"><a href=""><i class="fa fa-linkedin"></i></a></li>
+                        <li class="mx-2"><a href=""><i class="fa fa-instagram"></i></a></li>
+                    </ul>
+                </article>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+</section>
